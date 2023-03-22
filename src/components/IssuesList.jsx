a154a8787704cebc111d2680ca77b7eecb84de39
progali007 <code.progali@gmail.com>
2023-03-22 15:22:38 +0430

    Get issues list data

--- src/components/IssuesList.jsx
+++ src/components/IssuesList.jsx
@@ -1,14 +1,24 @@
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
+import IssueItem from "./IssueItem";
 
 export default function IssuesList() {
+  const issuesQuery = useQuery(["issues"], () =>
+    fetch("/api/issues").then((res) => res.json())
+  );
+
   return (
     <div>
-      <h1>Issues List</h1>
-      <ul>
-        <li>
-          <Link to="/issue/1">Issue 1</Link>
-        </li>
-      </ul>
+      <h2>Issues List</h2>
+      {issuesQuery.isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {issuesQuery.data?.map((issue) => (
+            <IssueItem key={issue?.id} issue={issue}/>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
