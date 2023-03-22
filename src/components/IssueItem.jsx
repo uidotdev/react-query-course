afca37aec8588e670d4e6fd79d897d40c4b11c8c
progali007 <code.progali@gmail.com>
2023-03-22 15:46:23 +0430

    Set issue title in a link component

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { GoIssueOpened, GoIssueClosed } from "react-icons/go";
+import { Link } from "react-router-dom";
 
 function IssueItem({ issue }) {
   const { title, labels, comments, number, createdDate, createdBy, assignee, status } = issue;
   return <li>
     <div>
       {status === "done" || status === "cancelled" ? <GoIssueOpened style={{color: "red"}}/> : <GoIssueClosed style={{color: "green"}}/>}
+    </div>
+    <div className="issue-content">
+      <span>
+        <Link to={`/issue/${number}`}>{title}</Link>
+      </span>
     </div>
   </li>;
 }
