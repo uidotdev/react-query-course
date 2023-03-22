746500dc8edd512e78635a639ecafd58d9ffef2a
progali007 <code.progali@gmail.com>
2023-03-22 15:59:45 +0430

    Add lable section and show created date of each issue

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -1,19 +1,43 @@
 import React from "react";
 import { GoIssueOpened, GoIssueClosed } from "react-icons/go";
 import { Link } from "react-router-dom";
+import { relativeDate } from "../helpers/relativeDate";
 
 function IssueItem({ issue }) {
-  const { title, labels, comments, number, createdDate, createdBy, assignee, status } = issue;
-  return <li>
-    <div>
-      {status === "done" || status === "cancelled" ? <GoIssueOpened style={{color: "red"}}/> : <GoIssueClosed style={{color: "green"}}/>}
-    </div>
-    <div className="issue-content">
-      <span>
-        <Link to={`/issue/${number}`}>{title}</Link>
-      </span>
-    </div>
-  </li>;
+  const {
+    title,
+    labels,
+    comments,
+    number,
+    createdDate,
+    createdBy,
+    assignee,
+    status,
+  } = issue;
+  return (
+    <li>
+      <div>
+        {status === "done" || status === "cancelled" ? (
+          <GoIssueOpened style={{ color: "red" }} />
+        ) : (
+          <GoIssueClosed style={{ color: "green" }} />
+        )}
+      </div>
+      <div className="issue-content">
+        <span>
+          <Link to={`/issue/${number}`}>{title}</Link>
+          {labels?.map((label) => (
+            <span key={label} className={`label red`}>
+              {label}
+            </span>
+          ))}
+        </span>
+        <small>
+          # {number} opened {relativeDate(createdDate)}
+        </small>
+      </div>
+    </li>
+  );
 }
 
 export default IssueItem;
